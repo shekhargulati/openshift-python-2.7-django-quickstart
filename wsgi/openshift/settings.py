50cbcaa7c276dc3f09d7ff18f98ee6e924d08159
Shekhar Gulati <shekhargulati84@gmail.com>
2013-07-25 14:19:56 +0530

    added 500.html and 404.html

--- wsgi/openshift/settings.py
+++ wsgi/openshift/settings.py
@@ -80,7 +80,10 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = os.path.join(PROJECT_DIR, '..', 'static')
+if 'OPENSHIFT_REPO_DIR' in os.environ:
+    STATIC_ROOT = os.path.join(os.environ.get('OPENSHIFT_REPO_DIR'), 'wsgi', 'static')
+else:
+    STATIC_ROOT = os.path.join(PROJECT_ROOT, STATIC_URL.strip("/"))
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
