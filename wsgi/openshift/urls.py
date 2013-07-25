b28660d5e8f1ffe511dbc6a80228d06902380a78
Shekhar Gulati <shekhargulati84@gmail.com>
2013-07-25 14:05:50 +0530

    added 500.html and 404.html

--- wsgi/openshift/urls.py
+++ wsgi/openshift/urls.py
@@ -1,4 +1,6 @@
+from django.conf import settings
 from django.conf.urls.defaults import patterns, include, url
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -16,11 +18,9 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
 )
 
-from django.conf import settings
-if settings.DEBUG:    
 urlpatterns += patterns('', (r'^media\/(?P<path>.*)$',
                                  'django.views.static.serve',
                                  {'document_root': settings.MEDIA_ROOT}),
                            )
-    from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-    urlpatterns += staticfiles_urlpatterns()
+
+urlpatterns += staticfiles_urlpatterns()
