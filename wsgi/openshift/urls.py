48f5470ad59de5fd510e531907f0bcbc6e78a78a
Shekhar Gulati <shekhargulati84@gmail.com>
2013-07-25 13:56:32 +0530

    added 500.html and 404.html

--- wsgi/openshift/urls.py
+++ wsgi/openshift/urls.py
@@ -6,11 +6,11 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    url(r'^$', 'openshift.views.home', name='home'),
+    url(r'^$', 'home', name='home'),
     # url(r'^openshift/', include('openshift.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
-    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    #url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
